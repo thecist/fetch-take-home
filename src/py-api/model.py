43d8fcc78aab3faf61fd16406dad37c10d884455
Tayomide <tibrahim@hawk.iit.edu>
2025-05-29 19:49:38 -0400

    Fix Syntax Error
    Generated model produced yellow squiggly line due to this error "Call expression not allowed in type expression"

--- src/py-api/model.py
+++ src/py-api/model.py
@@ -9,35 +9,41 @@ from typing import List
 
 from pydantic import BaseModel, Field, constr
 
-
 class Item(BaseModel):
-    shortDescription: constr(regex=r'^[\w\s\-]+$') = Field(
-        ...,
-        description='The Short Product Description for the item.',
-        example='Mountain Dew 12PK',
-    )
-    price: constr(regex=r'^\d+\.\d{2}$') = Field(
-        ..., description='The total price payed for this item.', example='6.49'
-    )
+  shortDescription: constr = Field(
+    ...,
+    description='The Short Product Description for the item.',
+    example='Mountain Dew 12PK',
+    regex=r'^[\w\s\-]+$'
+  )
+  price: constr = Field(
+    ...,
+    description='The total price payed for this item.',
+    example='6.49',
+    regex=r'^\d+\.\d{2}$'
+  )
 
 
 class Receipt(BaseModel):
-    retailer: constr(regex=r'^[\w\s\-&]+$') = Field(
-        ...,
-        description='The name of the retailer or store the receipt is from.',
-        example='M&M Corner Market',
-    )
-    purchaseDate: date = Field(
-        ...,
-        description='The date of the purchase printed on the receipt.',
-        example='2022-01-01',
-    )
-    purchaseTime: time = Field(
-        ...,
-        description='The time of the purchase printed on the receipt. 24-hour time expected.',
-        example='13:01',
-    )
-    items: List[Item] = Field(..., min_items=1)
-    total: constr(regex=r'^\d+\.\d{2}$') = Field(
-        ..., description='The total amount paid on the receipt.', example='6.49'
-    )
+  retailer: constr = Field(
+    ...,
+    description='The name of the retailer or store the receipt is from.',
+    example='M&M Corner Market',
+    regex=r'^[\w\s\-&]+$'
+  )
+  purchaseDate: date = Field(
+    ...,
+    description='The date of the purchase printed on the receipt.',
+    example='2022-01-01'
+  )
+  purchaseTime: time = Field(
+    ...,
+    description='The time of the purchase printed on the receipt. 24-hour time expected.',
+    example='13:01'
+  )
+  items: List[Item] = Field(..., min_items=1)
+  total: constr = Field(
+    ..., description='The total amount paid on the receipt.',
+    example='6.49',
+    regex=r'^\d+\.\d{2}$'
+  )
