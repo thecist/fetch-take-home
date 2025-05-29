c05b0a1e37699a3a6df8c7c9fece0661e6b8afc0
Tayomide <tibrahim@hawk.iit.edu>
2025-05-29 19:23:53 -0400

    Create fallback handler
    This commit creates a custom fallback handler for out of bound routes

--- src/py-api/app.py
+++ src/py-api/app.py
@@ -1,6 +1,8 @@
 import uvicorn
 import os
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 PORT = int(os.getenv("PORT", 8000))
@@ -12,8 +14,19 @@ PORT = int(os.getenv("PORT", 8000))
 async def root():
   return {"message": "Hello World"}
 
-#TODO: Create a fallback route to handle any other requests - something like 404
-#not found or route not required for assesment (something funny but professional)
+# Fallback route handler for 404s
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
+  if exc.status_code == 404:
+    return JSONResponse(
+      status_code=404,
+      content={
+        "error": "Route not found",
+        "message": "This endpoint isn't part of the take-home assessment. [Insert cute cat gif here]"
+      },
+    )
+  return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
 
 if __name__ == "__main__":
   uvicorn.run("app:app", host="0.0.0.0", port=PORT, reload=True)
